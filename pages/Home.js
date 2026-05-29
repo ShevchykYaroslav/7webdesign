1cdb31a841c30e99d0b0dd95a9d426df5cc2f3c2
ShevchykYaroslav <shevchyk.yar@e-u.edu.ua>
2026-05-29 06:35:28 +0300

    upd2

--- pages/Home.js
+++ pages/Home.js
@@ -3,19 +3,17 @@ export const HomePage = () => {
         <section id="home" class="hero" style="animation: fadeIn 0.5s;">
             <h1>Керуйте своїм Discord сервером легко</h1>
             <p>Універсальний бот для модерації, музики та розваг.</p>
-
-            <div style="margin-top: 2rem; text-align: center;">
+            
+            <div style="margin-top: 2rem;">
                 <img 
                     src="my-image.webp" 
                     alt="Інтерфейс LimbBot" 
                     loading="lazy" 
-                    width="800" 
+                    width="400" 
                     height="400"
-                    style="max-width: 100%; height: auto; border-radius: 8px;"
+                    style="max-width: 100%; height: auto; display: block; margin: 0 auto; aspect-ratio: 1 / 1; object-fit: contain; border-radius: 8px;"
                 >
             </div>
         </section>
-
-
     `;
 };
